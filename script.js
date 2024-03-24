e578d0a22981d65ed1e6ff9cd858f76d168d96c4
Kristina <krismetodieva123@gmail.com>
2024-03-24 12:22:54 +0200

    scroll to top button fixed but the footer broke

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const toTop = document.querySelector('#to-top');
+// const toTop = document.querySelector('#to-top');
 
-window.addEventListener("scroll", () => {
-    if(window.screenY > 100) {
-        toTop.classList.add("active");
-    } else {
-        toTop.classList.remove("active");
-    }
-});
+// window.addEventListener("scroll", () => {
+//     if(window.screenY > 100) {
+//         toTop.classList.add("active");
+//     } else {
+//         toTop.classList.remove("active");
+//     }
+// });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("scrollToTopBtn").style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0; /* For Safari */
+  document.documentElement.scrollTop = 0; /* For Chrome, Firefox, IE, and Opera */
+}
